71a8e3f3fb64b11e908b268f0b729e7777227c27
ChasTechProjects <44618658+ChasTechProjects@users.noreply.github.com>
2021-10-28 11:14:07 +0100

    Add Telephony files
    This might (not yet tested) fix the SIM card not being detected on the LTE model (TB8704X).

--- TB8704/device.mk
+++ TB8704/device.mk
@@ -51,3 +51,8 @@ PRODUCT_COPY_FILES += \
 
 # Inherit from tb-common
 $(call inherit-product, device/lenovo/tb-common/tb.mk)
+
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.gsm.xml \
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.telephony.ims.xml
